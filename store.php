2f8d416afccfb94ae98192b352ec10188a5a93a0
Monir EL OUARROUDI <95141609+ElouarrM@users.noreply.github.com>
2022-06-08 21:00:52 +0200

    Create store.php

---
+++ store.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stores</title>
+    <link rel="stylesheet" href="css/store.css">
+</head>
+<heeder>
+    <div class="head">
+    <l1>Bienvenue, Zouhir el amraani<br></l1>
+    <l2>Store </l2>
+</div>
+</header>
+<body>
+    <nav class="side-nav">
+        <div class="zoom">
+
+        <ul>
+            <li id="store" class="icon"><a href="store.php"><img src="img/store.png" alt="" width="30px"></a></li><div class="icon-store">Store</div>
+            <li id="users" class="icon"><a href="users.php"><img src="img/users.png" alt="" width="30px"></a></li><div class="icon-users">Users</div>
+            <li id="info" class="icon"><a href="#"><img src="img/info.png" alt="" width="30px"></a></li><div class="icon-info">Logs</div>
+            <li id="logout" class="icon"><a href="logout.php"><img src="img/logout.png" alt="" width="30px"></a></li><div class="icon-logout">Logout</div>
+        </ul>
+    </div>
+    </nav>
+    <main>
+        <div class="box-add">
+            <form action="">
+                <div class="image"><img src="img/add.png" width="30px" ></div>
+            </form>
+</div>
+        <div class="box-moments">
+             <form action="">
+                 <div class="info1">
+                 <spam>Moments one</spam>
+                 <spam>Nador</spam>
+                 <spam>jkhgfdfghijokojiugyfdtfyuiopu<br>kytdfhjkjhy9ob<br>kuytydrpl,mkjbhvguipklmkbjvhfg<br> </spam>
+                </div>            
+                </form>
+            
+        </div>
+        <div class="box-moments">
+             <form action="">
+                 <div class="info1">
+                 <label for="titre">
+                    <input id="titre" type="text" placeholder="Titre" name="#" required>
+                </label><br>
+
+                <label for="location">
+                    <input id="location" type="text" placeholder="Location" name="#" required>
+                </label><br>
+                <label for="desc">
+                    <input id="desc" type="text" placeholder="description" name="#" required>
+                </label>         
+                </form>
+            
+        </div>
+        </div>
+
+        <div class="box-moments">
+             <form action="">
+                 <div class="info1">
+                 <spam>Moments one</spam>
+                 <spam>Nador</spam>
+                 <spam>jkhgfdfghijokojiugyfdtfyuiopu<br>kytdfhjkjhy9ob<br>kuytydrpl,mkjbhvguipklmkbjvhfg<br> </spam>
+                </div>            
+                </form>
+            
+        </div>
+        
+      
+
+        
+        
+    </main> 
+</body>
+</html>
