e3547741b909cbafbb0a86fbc99781768d965f69
abdouaaba <90163036+abdouaaba@users.noreply.github.com>
2022-06-11 00:51:53 +0100

    Update store.php

--- store.php
+++ store.php
@@ -1,4 +1,3 @@
-<!--
 <?php
 session_start();
 
@@ -6,9 +5,10 @@ session_start();
     include("functions.php");
 
     $user_data= check_login($con);
+    echo $user_data['id'];
 
 ?>
--->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,7 +18,7 @@ session_start();
     <title>Stores</title>
     <link rel="stylesheet" href="css/store.css">
 </head>
-<heeder>
+<header>
     <div class="head">
     <l1>Bienvenue, Zouhir el amraani<br></l1>
     <l2>Store </l2>
@@ -38,111 +38,80 @@ session_start();
     </nav>
     <main>
         <div id="box-add">
-                <div id="add-image"><img src="img/add.png" width="30px" ></div>
+                <div id="add-image"><img src="img/add.png" width="50px" ></div>
                 
-                <form action="" id="create-form">
+                <form action="create.php" method="POST" id="create-form" enctype="multipart/form-data">
                     <div class="titre-add">
-                        <input class="titre-input-add" type="text" placeholder="Titre" name="titre" required>
+                        <input class="titre-input-add" type="text" placeholder="Titre" name="title" required>
                     </div>
                     <div class="location-add">
                         <input class="location-input-add" type="text" placeholder="Location" name="location" required>
                     </div>
                     
                     <div class="description-add">
-                        <textarea class="description-input-add" name="description" rows="13" cols="48" required>qsjkdfhsdqjfhqsdkjfhqsdlkjfhqslkdqskjdlqksjdlkqsjdlkqsjdlkqsjdlkqsjdlkqsjdlkqjsd</textarea>
+                        <textarea class="description-input-add" name="description" rows="13" cols="48" required></textarea>
                     </div>
                     
                     <div class="image-add">
-                        <input class="image-input-add" type="file"  name="image" required>
+                        <input class="image-input-add" type="file"  name="picture" required>
                     </div>
                     
                     <div class="prix-add">
-                        <input class="prix-input-add" type="text" placeholder="Price" name="prix" required>
+                        <input class="prix-input-add" type="text" placeholder="Price" name="price" required>
                     </div>
                     <div class="copies-add">
-                        <input class="copies-input-add" type="text" placeholder="Copies" name="copies" required>
+                        <input class="copies-input-add" type="text" placeholder="Number of Copies" name="nbCopies" required>
                     </div>
-                    <button type="submit" name="create" id="create-button">✓</button>
+                    <button type="submit" name="create" id="create-button"><img src="img/submit.png" alt="" width="20px"></button>
                 </form>
-        </div>
-        <div class="box-monum" style="background-image: url(img/bg.jpg); background-position: center; background-repeat: no-repeat; background-size: auto 400px;">
-            <form action="" class="infos">
-                <div class="titre">
-                    <input class="titre-input" type="text" placeholder="Titre" name="titre" readonly>
-                </div>
-                <div class="location">
-                    <input class="location-input" type="text" placeholder="Location" name="location" readonly>
-                </div>
-                
-                <div class="description">
-                    <textarea class="description-input" name="description" rows="15" cols="48" readonly>qsjkdfhsdqjfhqsdkjfhqsdlkjfh</textarea>
-                </div>
-                <!--
-                <div class="image">
-                    <input class="image-input" type="file"  name="image" readonly>
-                </div>
-                -->
-                <div class="prix">
-                    <input class="prix-input" type="text" placeholder="Price" name="prix" readonly>
-                </div>
-                <div class="copies">
-                    <input class="copies-input" type="text" placeholder="Copies" name="copies" readonly>
-                </div>
-                </form>
-            
-        </div>
-        <div class="box-monum" style="background-image: url(img/bg.jpg); background-position: center; background-repeat: no-repeat; background-size: auto 400px;">
-            <form action="" class="infos">
-                <div class="titre">
-                    <input class="titre-input" type="text" placeholder="Titre" name="titre" readonly>
-                </div>
-                <div class="location">
-                    <input class="location-input" type="text" placeholder="Location" name="location" readonly>
-                </div>
-                
-                <div class="description">
-                    <textarea class="description-input" name="description" rows="15" cols="48" readonly>qsjkdfhsdqjfhqsdkjfhqsdlkjfh</textarea>
-                </div>
-                <!--
-                <div class="image">
-                    <input class="image-input" type="file"  name="image" readonly>
-                </div>
-                -->
-                <div class="prix">
-                    <input class="prix-input" type="text" placeholder="Price" name="prix" readonly>
-                </div>
-                <div class="copies">
-                    <input class="copies-input" type="text" placeholder="Copies" name="copies" readonly>
-                </div>
-                </form>
-            
         </div>
 
-        <div class="box-monum" style="background-image: url(img/bg.jpg); background-position: center; background-repeat: no-repeat; background-size: auto 400px;">
-            <form action="" class="infos">
+        <?php
+        
+        $records = mysqli_query($con,"select id_monument, title, place, description, image, image_path, price, copies from monuments where user_id = '".$user_data['id']."';");
+
+        while($data = mysqli_fetch_array($records))
+        {
+        ?>
+
+        <div id=<?php echo $data['id_monument']; ?> class="box-monum" style="background-image: url(<?php echo $data['image_path']; ?>); background-position: center; background-repeat: no-repeat; background-size: auto 400px;">
+            <form action=<?php echo "update.php?did=".$data['id_monument']; ?> method="POST" class="infos" enctype="multipart/form-data">
                 <div class="titre">
-                    <input class="titre-input" type="text" placeholder="Titre" name="titre" readonly>
+                    <input class="titre-input input-field" type="text" value=<?php echo $data['title']; ?> name="title" readonly>
                 </div>
                 <div class="location">
-                    <input class="location-input" type="text" placeholder="Location" name="location" readonly>
+                    <input class="location-input input-field" type="text" value=<?php echo $data['place']; ?> name="location" readonly>
                 </div>
                 
                 <div class="description">
-                    <textarea class="description-input" name="description" rows="15" cols="48" readonly>qsjkdfhsdqjfhqsdkjfhqsdlkjfh</textarea>
+                    <textarea class="description-input input-field" name="description" rows="13" cols="48" readonly><?php echo $data['description']; ?></textarea>
+                </div>
+                
+                <div class="image">
+                    <input class="image-input" type="file"  name="picture" readonly>
                 </div>
                 
                 <div class="prix">
-                    <input class="prix-input" type="text" placeholder="Price" name="prix" readonly>
+                    <input class="prix-input input-field" type="text" value=<?php echo $data['price'].'$';?> name="price" readonly>
                 </div>
                 <div class="copies">
-                    <input class="copies-input" type="text" placeholder="Copies" name="copies" readonly>
+                    <input class="copies-input input-field" type="text" value=<?php echo $data['copies']; ?> name="nbCopies" readonly>
                 </div>
-                </form>
-            
+                <div class="edit-button"><img src="img/edit.png" alt="" width="20px"></div>
+                <div class="delete-button"><img src="img/delete.png" alt="" width="20px"></div>
+                <button type="submit" name="update" class="confirm-button"><img src="img/submit.png" alt="" width="20px"></button>
+            </form>
         </div>
+
+        <?php
+        }
+        ?>
+        
+        
     
     </main>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script> 
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
         var addButton = document.getElementById('add-image');
 
@@ -155,13 +124,75 @@ session_start();
         })
     </script>
     <script>
+
+        //hover effect
         $(".box-monum").hover(function(){
-        $(this).find(".infos").stop(true,true).css({"left":"20px"}).animate({"top":"-10px", "opacity": "1"},200);
+        $(this).find(".infos").animate({"opacity": "1"},200);
         },function(){
-        $(this).find(".infos").animate({"top":"-20px", "opacity": "0"},200, function() {
-            $(this).css({"left":"-9999px", "top":"0"});
+        $(this).find(".infos").animate({"opacity": "0"},100);
         });
+
+
+        //edit button
+        $(".edit-button").on('click', function(e){
+            $(this).hide();
+
+            var deleteButton = $(this).siblings('.delete-button');
+            var confirmButton = $(this).siblings('.confirm-button');
+            var imageInput = $(this).siblings('.image');
+            deleteButton.css("left", "17.5em");
+            confirmButton.show();
+            imageInput.show();
+
+            var formParent = $(this).parent();
+
+            var inputFields = formParent.find(".input-field")
+            inputFields.css({"text-shadow": "none", "cursor": "text"});
+            inputFields.attr("readonly", false).css("background", "rgb(175, 175, 175)").focus(function() {
+                $(this).css("background", "#7049bd");
+            }).blur(function() {
+                $(this).css("background", "rgb(175, 175, 175)");
+            });
+
         });
+
+        //delete button with confirmation
+        $('.delete-button').on('click', function(e) {
+            e.preventDefault();
+            var self = $(this);
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        var boxParent = $(this).parent().parent();
+                        var id = boxParent.attr('id');
+
+                        $.ajax({
+                            url: 'delete.php',
+                            type: 'POST',
+                            dataType: 'JSON',
+                            data: {id : id},
+                            fail: function(response) {
+                                alert(response.message);
+                            }
+                        });
+
+                        boxParent.fadeOut("fast");
+                        Swal.fire(
+                        'Deleted!',
+                        'Deleted succefully.',
+                        'success'
+                        )
+                    }
+            })
+        })
     </script>
+    
 </body>
 </html>
